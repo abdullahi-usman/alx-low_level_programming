11002aa18b822a5d587d2cbd590fad594e95a289
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-10 07:13:35 +0100

    putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,10 +6,17 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
-	int n;
-	for(n = 97; n <= 122; n++)
+{
+	int n, nn;
+
+	for (n = 97; n <= 123; n++)
+	{
+		nn = n;
+		if (n == 123)
+			nn = 40;
+
 		putchar(n);
+	}
 
 	return (0);
 }
