16d0ee6cf5d8f7c0c88686f2ec7a8e39dc6a24bd
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-13 22:28:26 +0100

    updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
 void _putchar(char ch);
-
+/*
+ * Prints alphabets
+ *
+ */
 extern void print_alphabet(void)
 {
 	int c;
 
-	for (c=97; c<=122; c++)
+	for (c = 97; c <= 122; c++)
 	{
 		_putchar(c);
 	}
+
+	_putchar(c);
 }
