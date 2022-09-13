fa1ae04d5513131f7d57070d2329300030340654
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-13 23:38:20 +0100

    updated

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "putchar.h"
+
+**
+ * print_alphabet_x10 - Prints the alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+    int s;
+    for (s = 0; s < 10; s++)
+    {
+        int c;
+        
+        for (c = 97; c <= 122; c++)
+        {
+            _putchar((char)c);
+        }
+    }
+
+	_putchar('\n');
+}
