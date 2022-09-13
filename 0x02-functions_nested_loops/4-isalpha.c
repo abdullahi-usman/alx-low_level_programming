20abf1ae1897600d8e68a8b2f800a0079b8cdc6d
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-14 00:24:25 +0100

    updated

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,11 @@
 /**
- * _islower - _islower
+ * _isalpha - _isalpha
  * @c: the character
  * Return: Always 0 (Success)
  */
 int _isalpha(int c)
 {
-    if ((c < 65 || c > 122) || (c > 90 && c < 97))
-        return (0);
-
-    return (1);
+	if ((c < 65 || c > 122) || (c > 90 && c < 97))
+		return (0);
+	return (1);
 }
