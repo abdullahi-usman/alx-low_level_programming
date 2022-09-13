5bacd295342c6a6eafd2cec457efc7798021270e
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-13 23:46:58 +0100

    updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,16 @@
 void print_alphabet_x10(void)
 {
     int s;
+
     for (s = 0; s < 10; s++)
     {
         int c;
+
         for (c = 97; c <= 122; c++)
         {
             _putchar((char)c);
         }
+
         _putchar('\n');
     }
 }
