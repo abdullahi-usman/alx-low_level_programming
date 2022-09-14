76605b34d5b9d407dce1cd15953c46bd761279f2
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-14 01:06:00 +0100

    updated

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,17 +4,14 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
-		//		putchar('-');
 		return -1;
 	}
 	else if (n > 0)
 	{
-		//		putchar('+');
 		return 1;
 	}
 	else
 	{
-		//		putchar('0');
 		return 0;
 	}
 }
