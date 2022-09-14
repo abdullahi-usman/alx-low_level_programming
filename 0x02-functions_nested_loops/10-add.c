33c93004d375551c072679beca6bd090c92207e7
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-14 02:47:35 +0100

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,9 @@
+/**
+ * add - addtwo numbers
+ *
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,3 +10,4 @@ int _abs(int n);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
