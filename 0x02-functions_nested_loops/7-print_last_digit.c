cbef828198e2f6190213150685d01ce3a02597a7
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-14 01:39:37 +0100

    abs

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,11 @@
 int print_last_digit(int n)
 {
 	char buf[256];
+	char last_digit;
 
-	sprintf(buf, "%d", n);
-	return buf[strlen(buf) - 1];
+	sprintf(buf, "%c", n);
+	last_digit = buf[strlen(buf) - 1];
+
+	_putchar(last_digit);
+	return (last_digit);
 }
