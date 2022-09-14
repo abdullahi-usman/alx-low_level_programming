ca735954ff91f0c908c90eff6c5082d15311de96
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-14 03:05:31 +0100

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * print_to_98 - print_to_98 numbers
+ * @n: number
+ *
+ */
 void print_to_98(int n)
 {
 	int range_start = n;
@@ -12,5 +17,5 @@ void print_to_98(int n)
 			printf("%d, ", range_start++);
 	}
 
-	printf("%d", range_start);
+	printf("%d ", range_start);
 }
