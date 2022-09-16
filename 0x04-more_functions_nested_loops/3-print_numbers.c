b6bcad50bf35b4ccc0fbb409eada50013d7461fe
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-16 03:45:58 +0100

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "putchar.h"
+
+/**
+ * print_numbers - print numbers
+ *
+ * Return: Always status.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+}
