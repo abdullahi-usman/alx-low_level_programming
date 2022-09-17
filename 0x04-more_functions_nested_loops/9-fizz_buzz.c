7d668b9924d61fc042601e2222e0b4a2ea38198d
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-17 06:04:57 +0100

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 
 	int i;
 
-	for (i = 0; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
@@ -30,7 +30,7 @@ int main(void)
 			printf("%d", i);
 		}
 
-		if (i != 99)
+		if (i != 100)
 			printf(" ");
 	}
 
