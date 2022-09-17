228527de306a6a6bb3e48235173f76e2c8986046
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-17 05:47:56 +0100

    print #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "putchar.h"
+
+/**
+ * print_square(int size) - print squares
+ * @n: the number of # char
+ *
+ * Return: Always status.
+ */
+
+void print_square(int size)
+{
+
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
