5e4a2237519d2e3de6194668c22c802e6e11ad61
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-17 05:21:48 +0100

    print _

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "putchar.h"
+
+/**
+ * print_line - print line
+ * @n: the number of _ char
+ *
+ * Return: Always status.
+ */
+
+void print_line(int n)
+{
+
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        _putchar('_');
+    }
+
+    _putchar('\n');
+}
