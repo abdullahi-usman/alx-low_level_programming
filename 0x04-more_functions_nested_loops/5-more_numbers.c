6296649af6f033a2ec792536d683a0884912ea34
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-17 05:10:42 +0100

    print more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "putchar.h"
+
+/**
+ * more_numbers - print more numbers
+ *
+ * Return: Always status.
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar((char)i);
+		}
+	}
+
+	_putchar('\n');
+}
