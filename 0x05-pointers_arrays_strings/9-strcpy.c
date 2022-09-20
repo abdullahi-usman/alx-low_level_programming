4e765359214da7f8490774c1504b442d6889fce8
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-20 04:47:54 +0100

    copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _strcpy - copy str to dest
+ * @dest: the dest
+ * @src: the src
+ *
+ * Return: new string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
