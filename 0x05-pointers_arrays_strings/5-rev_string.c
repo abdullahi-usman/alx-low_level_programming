77912e9d0fea2c7da3f723472bedc939fc2b42f6
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-20 03:56:06 +0100

    return reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * rev_string - return string in reverse order
+ * @s: the string
+ *
+ * Return: the reverse string
+ *
+ */
+void rev_string(char *s)
+{
+	int s_length = strlen(s);
+	char *rev = malloc(sizeof(char) * strlen(s));
+	int i, j = 0;
+
+	for (i = s_length - 1; i >= 0; i--)
+	{
+		rev[j++] = s[i];
+	}
+
+	rev[s_length] = '\0';
+
+	return (rev);
+}
