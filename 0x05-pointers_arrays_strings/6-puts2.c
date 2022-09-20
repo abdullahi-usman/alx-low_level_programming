b4a427d8f784d01a0fd13de5a384006c15a58522
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-20 04:14:28 +0100

    put alt chars

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+/**
+ * puts2 - put alt character
+ * @str: the string
+ *
+ * Return: Void
+ */
+
+void puts2(char *str)
+{
+	int s_length = strlen(str);
+	int i;
+
+	for (i = 0; i < s_length; i++)
+	{
+		if (i % 2 == 0)
+			continue;
+
+		putchar(str[i]);
+	}
+}
