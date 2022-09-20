7caa6cc2399fa128ed84a3bd56420a24fee0e974
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-20 03:48:58 +0100

    prints reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_rev - print string in reverse order
+ * @s: the string
+ *
+ * Return: Void
+ *
+ */
+void print_rev(char *s)
+{
+	int s_length = strlen(s);
+	char rev[s_length + 1];
+	int i, j = 0;
+
+	for (i = s_length - 1; i >= 0; i--)
+	{
+		rev[j++] = s[i];
+	}
+
+	rev[s_length] = '\0';
+	puts(rev);
+}
