a4b46fe451ee5bd9f788a9792971b166641e8a0a
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-22 13:27:11 +0100

    fix code

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,16 +13,17 @@ char *cap_string(char *str)
 
 	int i;
 	int capitalize_next = -1;
+	char c;
 
-	for (i = 0; i < strlen(str); i++)
+	for (i = 0; i < (int)strlen(str); i++)
 	{
 		if (capitalize_next == 0)
 		{
-			str[i] = to_upper(str[i]);
+			str[i] = toupper(str[i]);
 			capitalize_next = -1;
 		}
 
-		char c = str[i];
+		c = str[i];
 
 		if (c == ',' || c == ';' || c == ' ' || c == '\n' ||
 			c == '\t' || c == '.' || c == '!' || c == '?' ||
