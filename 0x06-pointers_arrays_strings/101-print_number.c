78e5e1b944c6373cb5b639c65aa0cd58dd587b07
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-22 22:53:29 +0100

    added print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,18 @@
+#include "putchar.h"
+
+/**
+ * print_number - print the number
+ * @n: the number
+ */
+void print_number(int n)
+{
+	int i;
+	char buf[256];
+
+	sprintf(buf, "%d", n);
+
+	for (i = 0; i < (int)strlen(buf); i++)
+	{
+		_putchar(buf[i]);
+	}
+}
