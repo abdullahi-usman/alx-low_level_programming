96219f46dba289a8301c932409d5c97d8df3f24f
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-22 05:42:11 +0100

    added strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _strcmp - compare two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: The new string
+ */
+
+int _strcmp(char *dest, char *src)
+{
+	return (strcmp(dest, src));
+}
