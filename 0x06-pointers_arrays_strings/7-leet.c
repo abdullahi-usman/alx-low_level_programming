eb2a9bbc1f4827e1f131e4b2f7c017924d129b76
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-22 19:39:24 +0100

    added leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+
+
+/**
+ * leet - convert to code
+ * @str: the string
+ *
+ * Return: the new converted string
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	char *ch = "aeotlAEOTL";
+	char *encode = "4307143071";
+
+	for (i = 0; i < (int)strlen(ch); i++)
+	{
+		for (j = 0; j < (int)strlen(str); j++)
+		{
+			if (ch[i] == str[j])
+			{
+				str[j] = encode[i];
+			}
+		}
+	}
+
+	return (str);
+}
