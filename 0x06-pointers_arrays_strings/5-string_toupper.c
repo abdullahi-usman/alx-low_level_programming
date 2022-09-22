b6f55d9eb842adde196c106d75b19fb53b5f94a7
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-22 06:43:21 +0100

    add string to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * string_toupper - convert string to upper case
+ * @str: the string
+ *
+ * Return: the string in upper case
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; i < strlen(str); i++)
+	{
+		str[i] = toupper(str[i]);
+	}
+
+	return (str);
+}
