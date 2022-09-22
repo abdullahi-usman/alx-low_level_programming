6c241a83a37a8734139a5b63fcceccae8f970cf7
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-22 05:09:25 +0100

    concat two string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _strcat - concat two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: The new string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
