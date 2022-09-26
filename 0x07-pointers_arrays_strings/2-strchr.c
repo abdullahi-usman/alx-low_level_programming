f30399e125a0722ea5bc0b508415f0d64415e958
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-26 23:09:44 +0100

    added strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strchr - stringy character
+ * @s: the string
+ * @n: the character
+ *
+ * Return: the character
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
