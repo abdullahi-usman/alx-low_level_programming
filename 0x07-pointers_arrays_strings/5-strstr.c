2245eff073cc5269a57a5dbd5fe02069c9feddfc
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-26 23:23:08 +0100

    added strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _strstr - the string string
+ * @haystack: the stack
+ * @needle: the needle
+ *
+ *
+ * Return: the string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
