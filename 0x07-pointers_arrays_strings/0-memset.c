a234bd11118dc43881517092c682fb63cdfc3256
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-26 23:00:42 +0100

    added memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,13 @@
 #include <string.h>
 
+/**
+ * _memset - set the memory
+ * @s: the buffer to set
+ * @b: the char to set
+ * @n: the size
+ *
+ * Return: The setted buffer
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	return (char *)memset(s, b, n);
