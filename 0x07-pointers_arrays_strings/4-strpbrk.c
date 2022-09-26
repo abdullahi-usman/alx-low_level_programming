77623102e8b4f54358925126e857483783c0e14a
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-26 23:19:42 +0100

    added strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _strpbrk - the string break
+ * @s: the string
+ * @accept: the break
+ *
+ * Return: the breakkaede string
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
