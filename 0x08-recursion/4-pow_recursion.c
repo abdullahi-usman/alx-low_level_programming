2ab0dac73bc5b839d02d9c93962425b92861cb68
Abdullahi Usman <abdullahismn7@gmail.com>
2022-09-29 02:30:16 +0100

    added pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include <math.h>
+
+/**
+ * _pow_recursion - recursion pow
+ * @x: the x value
+ * @y: the y value
+ *
+ * Return: the powed
+ */
+
+int _pow_recursion(int x, int y)
+{
+	return (pow(x, y));
+}
