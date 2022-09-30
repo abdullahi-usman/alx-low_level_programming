c359105abc39db9ff6c2f0272be140841b777d9b
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-01 00:19:17 +0100

    print number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the program entry point
+ * @argc: number of argumetns
+ * @argv: the array oc arguments
+ *
+ * Return: Vois
+ */
+
+int main(int argc, char **argv)
+{
+
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
