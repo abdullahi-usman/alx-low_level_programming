8b963445faa8dad6488ac4ab82b6882ef0e791e1
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-01 00:09:00 +0100

    print programs name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the program entry point
+ * @argc: number of argumetns
+ * @argv: the array oc arguments
+ *
+ * Return: Vois
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s", argv[0]);
+}
