90d0ef70393febeb306592ff525eeb4ce856ab3f
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-03 23:17:19 +0100

    add alloc grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * alloc_grid - alloc a two dimesion array
+ * @width: the width
+ * @height: the height
+ *
+ * Return: return a new array
+ */
+int **alloc_grid(int width, int height)
+{
+
+	int **array;
+
+	if (width == 0 || height == 0)
+		return (NULL);
+
+	array = (int **)calloc(width, sizeof(int) * height);
+
+	if (array == NULL)
+		return (array);
+
+	memset(array, (char)0, sizeof(width * height));
+
+	return (array);
+}
