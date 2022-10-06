e87e0c727ab7e95373bda6b3dbd4dc4461f3d25e
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-06 04:51:53 +0100

    argstr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,27 +10,23 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i;
+	int i, char_size = 0;
 	char *args, *n_args;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	args = (char *)malloc(ac * sizeof(char));
+	for (i = 0; i < ac; i++)
+	{
+		char_size += (int)strlen(av[i]);
+	}
+
+	args = (char *)malloc((ac + char_size) * sizeof(char));
 	if (args == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
 	{
-		n_args = (char *)realloc(args, (strlen(args) + strlen(av[i])) * sizeof(char));
-
-		if (n_args == NULL)
-		{
-			free(args);
-			return (NULL);
-		}
-		args = n_args;
-
 		strcat(args, av[i]);
 		strcat(args, "\n");
 	}
