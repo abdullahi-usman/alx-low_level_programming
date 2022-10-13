7d76ba129d36560977bb33143b80e03f65acfb9b
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-13 04:38:35 +0100

    add array_iterator to header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef __FUNCTION_POINTERS_H__
 #define __FUNCTION_POINTERS_H__
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
