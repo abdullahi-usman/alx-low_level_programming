7add184b3b1a0b2daf22d1e07701846887ef7604
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-13 04:25:51 +0100

    add print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+
+
+/**
+ * print_name - call the function to print name
+ * @name:  the function name
+ * @f: the function
+ *
+ * Return: Void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
