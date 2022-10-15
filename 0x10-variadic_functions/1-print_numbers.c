6b2c2a9cd1f52a7feef4c0ac6d85e22e56f1f106
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-15 01:38:54 +0100

    add variadic argument

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -26,10 +26,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			{
 				printf("%s", separator);
 			}
-			else
-			{
-				printf(" ");
-			}
+			printf(" ");
 		}
 		else
 		{
