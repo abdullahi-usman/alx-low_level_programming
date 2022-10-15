52aed06462badf536d17684ff7a65e2ef1b3fb30
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-15 01:34:32 +0100

    add variadic argument

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - print the variadic arg number
+ * @separator: the seperator between each number
+ * @n: number of argument
+ *
+ * Return: Void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	va_list ap;
+
+	va_start(ap, n);
+
+	for (i = 0; i < (int)n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+
+		if (i != (int)n - 1)
+		{
+			if (separator != NULL)
+			{
+				printf("%s", separator);
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,21 @@
 #ifndef __VARIADIC_FUNCTIONS_H__
 #define __VARIADIC_FUNCTIONS_H__
 
+/**
+ * sum_them_all - sum the numbers
+ * @n: the number of number
+ *
+ * Return: Void
+ */
 int sum_them_all(const unsigned int n, ...);
+
+/**
+ * print_numbers - print the variadic arg number
+ * @separator: the seperator between each number
+ * @n: number of argument
+ *
+ * Return: Void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
 #endif
