0b3ca315042c0584901585fb92ce8a6b0980a904
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-20 23:16:43 +0100

    add free nodes -- fix 7

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -17,13 +17,16 @@ void free_list(list_t *head)
 
 	while (cur_head->next != NULL)
 	{
-		temp_head = &(*cur_head->next);
+		temp_head = cur_head->next;
 		if (cur_head->str != NULL)
 			free(cur_head->str);
 
+		cur_head->next = NULL;
 		free(cur_head);
+
 		cur_head = temp_head;
 	}
 
 	free(cur_head->str);
+	cur_head->next = NULL;
 }
