555dca509a20aa3b25211abc7673328efa948171
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-20 21:25:09 +0100

    free everyone

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Free the list
+ * @head: the list
+ *
+ * Return: Void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp_head, *cur_head = head;
+
+	while (cur_head->next != NULL)
+	{
+		temp_head = cur_head->next;
+		free(cur_head);
+		cur_head = temp_head;
+	}
+
+	free(cur_head);
+}
