ca01aa6ff6ebf2adc5a18841406e56675cfa2062
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-20 21:16:27 +0100

    add new node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,29 +12,16 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *cur_pos = NULL;
-
-	if (*head == NULL)
-	{
-		*head = (list_t *)malloc(sizeof(list_t));
-		cur_pos = *head;
-	}
-	else
-	{
-		cur_pos = *head;
-		while (cur_pos->next != NULL)
-		{
-			cur_pos = cur_pos->next;
-		}
-
-		cur_pos->next = (list_t *)malloc(sizeof(list_t));
-		cur_pos = cur_pos->next;
-	}
+	list_t *cur_pos = (list_t *)malloc(sizeof(list_t));
+	list_t *swap = NULL;
 
 	if (cur_pos != NULL)
 	{
 		cur_pos->str = strdup(str);
 		cur_pos->len = strlen(str);
+		swap = *head;
+		cur_pos->next = swap;
+		*head = cur_pos;
 	}
 
 	return (cur_pos);
