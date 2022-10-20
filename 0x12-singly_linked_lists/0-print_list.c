5f3d09d48633c15730c055d50e9b8a23f9a939fa
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-20 20:18:55 +0100

    fix current error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,22 +10,27 @@
 size_t print_list(const list_t *h)
 {
 	size_t c = 0;
+	char *str;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
-			h->str = "(nil)";
+			str = "(nil)";
+		}
+		else
+		{
+			str = h->str;
 		}
 
-		printf("[%d] %s", h->len, h->str);
+		printf("[%d] %s\n", h->len, str);
+		c++;
 
 		if (h->next == NULL)
 			break;
 
 		h = h->next;
-		c++;
-	}
+		}
 
 	return (c);
 }
