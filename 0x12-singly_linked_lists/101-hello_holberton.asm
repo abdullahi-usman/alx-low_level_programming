17d47e3b9da74d1b4fd5250bf6e5c4d163f1ad32
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-21 21:24:48 +0100

    assembly -- 1

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,10 @@
+        global    main
+        extern    printf
 
-          global    start
-
-          section   .text
-start:    mov       rax, 0x02000004         ; system call for write
-          mov       rdi, 1                  ; file handle 1 is stdout
-          mov       rsi, message            ; address of string to output
-          mov       rdx, 16                 ; number of bytes
-          syscall                           ; invoke operating system to do the write
-          mov       rax, 0x02000001         ; system call for exit
-          xor       rdi, rdi                ; exit code 0
-          syscall                           ; invoke operating system to exit
-
-          section   .data
-message:  db        "Hello, Holberton", 10 
+        section   .text
+main:                                      ; This is called by the C library startup code
+        mov       rdi, message            ; First integer (or pointer) argument in rdi
+        call      printf                   ; puts(message)
+        ret                               ; Return from main back into C library wrapper
+message:
+        db        "Hello, Holberton", 0
