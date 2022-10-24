a605d0c94e2f8a91a44fe9bc2e7bffc183164ed1
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-25 00:22:29 +0100

    add print int

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - print the number of nodes
+ * @h: the linked list
+ *
+ * Return: the size;
+ */
+size_t print_listint(const listint_t *h)
+{
+	ssize_t size = 0;
+	listint_t *cur = h;
+
+	while (cur != NULL)
+	{
+		size++;
+
+		cur = cur->next;
+	}
+
+	printf("%d", size);
+
+	return (size);
+}
