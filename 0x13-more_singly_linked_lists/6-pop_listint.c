577c6e88e36a090edb19e812242c5a44cbb2035c
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-25 01:56:33 +0100

    pop up the current head

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * pop_listint - pop up the last item
+ * @head: the linked list
+ * Return: the n's data
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *cur_iter = *head;
+	int n = cur_iter->n;
+
+	*head = (*head)->next;
+	free(cur_iter);
+
+	return (n);
+}
