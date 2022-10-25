d57bcc31087012823144fd7b37450edd69fc81a6
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-25 01:02:05 +0100

    add int to list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,7 +11,14 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *node = (listint_t *)malloc(sizeof(listint_t));
 	node->n = n;
 
-	node->next = *head;
+	if (*head == NULL)
+	{
+		*head = node;
+	}
+	else
+	{
+		node->next = *head;
+	}
 
 	return (node);
 }
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - add a new node to the end of the list
+ * @head: the head
+ * @n: the number
+ * Return: the new list
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n);
+{
+	listint_t *cur_iter, *node = (listint_t *)malloc(sizeof(listint_t));
+	node->n = n;
+
+	if (*head == NULL)
+		*head = (listint_t *)malloc(sizeof(listint_t));
+
+	cur_iter = *head;
+	while (cur_iter->next != NULL)
+	{
+		cur_iter = cur_iter->next;
+	}
+
+	cur_iter->next = node;
+
+	return (*head);
+}
