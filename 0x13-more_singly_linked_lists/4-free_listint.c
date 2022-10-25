dd900b3eb6c9755ce88ca606b42c4193302fae57
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-25 01:32:40 +0100

    free everything - fix 2

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * free_listint - free the listint
+ * @head: the head of list
+ * Return: Void
+ */
 void free_listint(listint_t *head)
 {
 	if (head->next != NULL)
