be741c598e73a3d740d2497f68d6926271bc92c0
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-25 01:40:35 +0100

    free part two -- fix 4

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,6 +2,19 @@
 #include "lists.h"
 
 /**
+ * free_listint - free the listint
+ * @head: the head of list
+ * Return: Void
+ */
+void free_listint(listint_t *head)
+{
+	if (head->next != NULL)
+		free_listint(head->next);
+
+	free(head);
+}
+
+/**
  * free_listint2 - free the listint and set head to NULL
  * @head: the head of list
  * Return: Void
