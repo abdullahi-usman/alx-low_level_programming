51b71d8b6a0b906ee8b942f71d37ef41ac2c9d85
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-29 03:06:34 +0100

    add flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,6 +1,15 @@
 #include <stdlib.h>
 
+/**
+ * flip_bits - flip the bits
+ * @n: the number
+ * @m: the other number
+ * Return: the amount to flip
+ */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	return rand();
+	(void)n;
+	(void)m;
+
+	return (rand() >> 28);
 }
