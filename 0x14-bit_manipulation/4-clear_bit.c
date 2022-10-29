cb48438be3524020fa4a3902eda9342817bccf56
Abdullahi Usman <abdullahismn7@gmail.com>
2022-10-29 03:01:07 +0100

    recon

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,17 +10,17 @@
  */
 int _pow(int n)
 {
-    int r = 2, i;
+	int r = 2, i;
 
-    for (i = 1; i < n; i++)
-    {
-        r *= 2;
-    }
+	for (i = 1; i < n; i++)
+	{
+		r *= 2;
+	}
 
-    if (n == 0)
-        return (0);
+	if (n == 0)
+		return (0);
 
-    return (r);
+	return (r);
 }
 /**
  * binary_to_uint - convert binary ti decimal
@@ -30,28 +30,28 @@ int _pow(int n)
 
 unsigned int binary_to_uint(const char *b)
 {
-    int r = 0, c, len = 0, _rr;
+	int r = 0, c, len = 0, _rr;
 
-    if (b != NULL)
-    {
-        len = (int)strlen(b);
+	if (b != NULL)
+	{
+		len = (int)strlen(b);
 
-        for (c = 0; c < len; c++)
-        {
+		for (c = 0; c < len; c++)
+		{
 
-            _rr = (int)b[c] - 48;
+			_rr = (int)b[c] - 48;
 
-            if (_rr != 0 && _rr != 1)
-                return (0);
+			if (_rr != 0 && _rr != 1)
+				return (0);
 
-            r += (_rr * _pow(len - c - 1));
+			r += (_rr * _pow(len - c - 1));
 
-            if (c == (len - 1) && _rr == 1)
-                r += 1;
-        }
-    }
+			if (c == (len - 1) && _rr == 1)
+				r += 1;
+		}
+	}
 
-    return (r);
+	return (r);
 }
 
 /**
@@ -63,20 +63,20 @@ unsigned int binary_to_uint(const char *b)
  */
 int __divide(unsigned long int n, int *reminder)
 {
-    int r = n, c = 0;
+	int r = n, c = 0;
 
-    while (1)
-    {
-        if (r < 2)
-        {
-            *reminder = r;
-            break;
-        }
-        r = r - 2;
-        c++;
-    }
+	while (1)
+	{
+		if (r < 2)
+		{
+			*reminder = r;
+			break;
+		}
+		r = r - 2;
+		c++;
+	}
 
-    return (c);
+	return (c);
 }
 
 /**
@@ -87,36 +87,36 @@ int __divide(unsigned long int n, int *reminder)
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    char *buf = (char *)malloc(sizeof(char) * 250);
-    char *r_buf = NULL;
-    int i, r_count = 0, c = *n, r = 0, c_count = 0;
+	char *buf = (char *)malloc(sizeof(char) * 250);
+	char *r_buf = NULL;
+	int i, r_count = 0, c = *n, r = 0, c_count = 0;
 
-    buf = memset(buf, 0, 250);
-    while (1)
-    {
-        r = 0;
-        if (c <= 0)
-            break;
+	buf = memset(buf, 0, 250);
+	while (1)
+	{
+		r = 0;
+		if (c <= 0)
+			break;
 
-        c = __divide(c, &r);
+		c = __divide(c, &r);
 
-        if (r == 0)
-            buf[c_count] = '0';
-        else if (r == 1)
-            buf[c_count] = '1';
+		if (r == 0)
+			buf[c_count] = '0';
+		else if (r == 1)
+			buf[c_count] = '1';
 
-        c_count++;
-    }
+		c_count++;
+	}
 
-    r_buf = (char *)malloc(sizeof(char) * 250);
-    buf[strlen(buf) - index - 1] = '0';
+	r_buf = (char *)malloc(sizeof(char) * 250);
+	buf[strlen(buf) - index - 1] = '0';
 
-    for (i = c_count; i >= 0; i--)
-    {
-        r_buf[r_count] = buf[i];
-        r_count++;
-    }
+	for (i = c_count; i >= 0; i--)
+	{
+		r_buf[r_count] = buf[i];
+		r_count++;
+	}
 
-    *n = binary_to_uint(r_buf);
-    return (1);
+	*n = binary_to_uint(r_buf);
+	return (1);
 }
