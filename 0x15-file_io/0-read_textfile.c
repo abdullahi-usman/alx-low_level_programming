3e53f8a0697432fe7f5ea00336cb72e049cf405e
Abdullahi Usman <abdullahismn7@gmail.com>
2022-11-01 02:11:45 +0100

    get file and print

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * read_textfile - read a file
+ * @filename: the file name
+ * @letters: number of letters to read
+ * Return: the number of bytes read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, malloc_s = sizeof(char) * letters;
+	ssize_t read_no = 0;
+	char *buf;
+
+	if (filename == NULL || letters == 0)
+		return (read_no);
+
+	buf = (char *)malloc(malloc_s);
+	fd = open(filename, O_RDONLY);
+
+	if (fd != -1 && buf != NULL)
+	{
+		read_no = read(fd, buf, malloc_s);
+
+		if (read_no != -1)
+		{
+			puts(buf);
+			read_no /= sizeof(char);
+		}
+		else
+		{
+			read_no = 0;
+		}
+	}
+
+	return (read_no);
+}
