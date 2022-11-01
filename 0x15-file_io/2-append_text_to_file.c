f3c9ed8ea6c330000a75a30749d42644cc9132ad
Abdullahi Usman <abdullahismn7@gmail.com>
2022-11-01 03:44:34 +0100

    append to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,13 +3,13 @@
 #include <unistd.h>
 #include <string.h>
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-	if (text_content != NULL)
+	if (fd != -1 && text_content != NULL)
 	{
 		if (write(fd, text_content, strlen(text_content) * sizeof(char)) != -1)
 			return 1;
