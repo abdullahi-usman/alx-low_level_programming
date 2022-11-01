44017c52200f87a38c06864e242567aae8f7d58d
Abdullahi Usman <abdullahismn7@gmail.com>
2022-11-01 03:36:49 +0100

    create new file and append

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int create_file(const char *filename, char *text_content)
+{
+    int fd;
+
+    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+    if (text_content != NULL)
+    {
+        if (write(fd, text_content, strlen(text_content) * sizeof(char)) != -1)
+            return 1;
+    }
+
+    return (fd);
+}
