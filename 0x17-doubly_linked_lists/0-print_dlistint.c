aeab8aa3e47c6b179a95eb8d2bc9ee6a749a4ffe
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-08 22:01:21 +0100

    fix print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -15,7 +15,7 @@ size_t print_dlistint(const dlistint_t *h)
 	{
 		printf("%d\n", h->n);
 		c++;
-		next = h->prev;
+		next = h->next;
 	}
 	else
 	{
@@ -30,7 +30,7 @@ size_t print_dlistint(const dlistint_t *h)
 		}
 		c++;
 		printf("%d\n", next->n);
-		next = next->prev;
+		next = next->next;
 	}
 
 	return (c);
