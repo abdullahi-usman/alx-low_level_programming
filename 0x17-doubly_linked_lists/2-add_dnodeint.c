57db478e339263ff8757e4e76b6938f6a32a5a94
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-09 02:02:13 +0100

    add new node

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - add int to last item
+ * @head: the list
+ * @n: the item
+ * Return: the new node
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	new_node->n = n;
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	if ((*head) == NULL)
+	{
+		(*head) = new_node;
+	}
+	else
+	{
+		(*head)->prev = new_node;
+		new_node->next = *head;
+		*head = new_node;
+	}
+
+	return (new_node);
+}
