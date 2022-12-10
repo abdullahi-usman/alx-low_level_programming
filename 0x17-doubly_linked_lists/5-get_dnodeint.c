8908a3aee5077f94eb8b273f167793466c4b2dfd
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-10 05:07:57 +0100

    betty corrections

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,24 +1,22 @@
 #include <stdlib.h>
 #include "lists.h"
 
-
 /**
  * get_dnodeint_at_index - get node at index
  * @head: the head
  * @index: the index
- * 
  * Return: the node where n occurs
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int c = 0;
-    dlistint_t *next_item = head;
+	unsigned int c = 0;
+	dlistint_t *next_item = head;
 
-    while (c != index && next_item->next != NULL)
-    {
-        next_item = next_item->next;
+	while (c != index && next_item->next != NULL)
+	{
+		next_item = next_item->next;
 		c++;
-    }
+	}
 
-    return (next_item);   
+	return (next_item);
 }
