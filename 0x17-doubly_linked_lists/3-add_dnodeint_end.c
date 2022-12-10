abaa7e28f664fff06d0cd8acdbc053f6ec982842
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-10 04:35:02 +0100

    add to list end - fix 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -32,7 +32,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 			if ((*head)->next == NULL)
 			{
 				(*head)->next = new_node;
-				new_node->prev = (*head)->prev;			
+				new_node->prev = (*head)->prev;
 				(*head) = new_head;
 				break;
 			}
@@ -41,7 +41,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 				(*head) = (*head)->next;
 			}
 		}
-		
+
 	}
 
 	return (new_node);
