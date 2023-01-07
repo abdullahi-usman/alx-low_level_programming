afba1c2cd4ebb95d9742f36f0b33b7f29c6c20f8
Abdullahi Usman <abdullahismn7@gmail.com>
2023-01-07 20:30:02 +0100

    add set node on table - fix 9

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -40,7 +40,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			if (tmp_node->next == NULL)
 			{
 				tmp_node->next = node;
-				break;
 			}
 			tmp_node = tmp_node->next;
 		}
