6c1c9a7b4af17572790d427ef886f0369e4d4641
Abdullahi Usman <abdullahismn7@gmail.com>
2023-01-07 20:08:20 +0100

    add set node on table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,13 +12,12 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long index = key_index((const unsigned char *)key, ht->size);
+	long index = key_index((const unsigned char *)key, ht->size);
 	char *dup_key = (char *)malloc(sizeof(key) + 1);
 	char *dup_value = (char *)malloc(sizeof(value) + 1);
 	hash_node_t *tmp_node, *node = (hash_node_t *)malloc(sizeof(hash_node_t));
 
-	if (dup_key == NULL || dup_value == NULL ||
-		node == NULL || 0 > (long)index || ((long)index) > (long)ht->size)
+	if (dup_key == NULL || dup_value == NULL || node == NULL)
 		return (0);
 
 	strcpy(dup_key, key);
