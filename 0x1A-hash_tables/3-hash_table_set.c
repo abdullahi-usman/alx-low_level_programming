342667b87a8b9c8f60f693a985f799f34e81b705
Abdullahi Usman <abdullahismn7@gmail.com>
2023-01-07 19:01:43 +0100

    add hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,28 +32,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		tmp_node = ht->array[index];
-		if (strcmp(tmp_node->key, dup_key) == 0)
-			strcpy(tmp_node->value, dup_value);
-		else
+		while (tmp_node != NULL)
 		{
-			while (tmp_node != NULL)
+			if (tmp_node->next != NULL)
 			{
-				if (tmp_node->next != NULL)
+				if (strcmp(tmp_node->key, dup_key) == 0)
 				{
-					if (strcmp(tmp_node->key, dup_key) == 0)
-					{
-						strcpy(tmp_node->value, dup_value);
-						break;
-					}
-					tmp_node = tmp_node->next;
-					continue;
-				} else
-				{
-					tmp_node->next = node;
+					strcpy(tmp_node->value, dup_value);
 					break;
 				}
-				return (0);
+				tmp_node = tmp_node->next;
+				continue;
+			} else
+			{
+				tmp_node->next = node;
+				break;
 			}
+			return (0);
 		}
 	}
 	return (1);
